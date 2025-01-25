9b6aded26ee29dcc8f171d72e612cc886c5e9cba
Johan Swan <jswannieman@gmail.com>
2025-01-25 17:18:01 +1300

    Commit SQL-String-Functions SQL Component

--- SQL-String-Functions.sql
+++ SQL-String-Functions.sql
@@ -33,15 +33,5 @@ FROM
     
 /* SUBSTRING */
 
-SELECT 
-    SUBSTRING('We walk down the valley',
-        4,
-        9);
-        
-SELECT 
-    first_name,
-    last_name,
-    SUBSTRING(birth_date, 6, 2) AS birth_month
-FROM
-    employee_personal_details;
+SELECT SUBSTRING("We walk down the valley", 4, 9);
 
