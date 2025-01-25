0ecb87ff130a0c03b76c0dcacd72d8f3579b834d
Johan Swan <jswannieman@gmail.com>
2025-01-25 14:08:42 +1300

    Update SQL-Joins SQL Component

--- SQL-Joins.sql
+++ SQL-Joins.sql
@@ -27,13 +27,27 @@ CREATE TABLE IF NOT EXISTS employee_work_details (
     first_name VARCHAR(40),
     last_name VARCHAR(40),
     occupation VARCHAR(60),
-    salary INT
+    salary INT,
+    dept_id INT
 );
 
 INSERT INTO employee_work_details
-(id, first_name, last_name, occupation, salary)
-VALUES (01, "James", "Mower", "Business Analyst", 115000), (03, "John", "Algar", "Reporting Analyst", 105000),
-       (04, "Mary", "McCarthy", "IT Help Desk Engineer", 95000), (05, "Joyce", "Tanham", "Data Analyst", 125000);
+(id, first_name, last_name, occupation, salary, dept_id)
+VALUES (01, "James", "Mower", "Business Analyst", 115000, 03), (03, "John", "Algar", "Reporting Analyst", 105000, 07),
+       (04, "Mary", "McCarthy", "IT Help Desk Engineer", 95000, 01), (05, "Joyce", "Tanham", "Data Analyst", 125000, 04),
+       (06, "Anna", "Goodman", "Data Analyst", 125000, 04), (07, "Hank", "Opperman", "IT Help Desk Engineer", 90000, 01);
+
+DROP TABLE IF EXISTS employee_departments;
+
+CREATE TABLE IF NOT EXISTS employee_departments (
+    id INT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(80)
+);
+
+INSERT INTO employee_departments
+(id, department_name)
+VALUES (01, "IT Hardware and Software Solutions"), (02, "IT Systems Administration"), (03, "Business Department"), 
+       (04, "Data Division"), (05, "Database Department"), (06, "Operations"), (07, "Reporting Department");
 
 /* INNER JOIN EXAMPLE 1 */
 
@@ -73,8 +87,20 @@ FROM
 
 /* SELF JOIN */
 
-SELECT * FROM employee_personal_details emp1
-JOIN employee_personal_details emp2
-ON emp1.id + 1 = emp2.id;
+SELECT 
+    *
+FROM
+    employee_personal_details emp1
+        JOIN
+    employee_personal_details emp2 ON emp1.id + 1 = emp2.id;
 
-/* Thats all folks */
+/* JOIN MULTIPLE TABLES */
+
+SELECT 
+    *
+FROM
+    employee_personal_details emppers
+        INNER JOIN
+    employee_work_details empwork ON emppers.id = empwork.id
+        INNER JOIN
+    employee_departments empdept ON empdept.id = empwork.dept_id;
