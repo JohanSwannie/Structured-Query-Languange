1a3659bfad268137cb958bcea843c073bb72178e
Johan Swan <jswannieman@gmail.com>
2025-02-01 13:29:14 +1300

    Update SQL-Subqueries SQL Component

--- SQL-Subqueries.sql
+++ SQL-Subqueries.sql
@@ -1,5 +1,7 @@
 USE companyA;
 
+/* SUBQUERY EXAMPLE 1 */
+
 SELECT 
     *
 FROM
@@ -11,3 +13,18 @@ WHERE dept_id in
             employee_departments
         WHERE
             dept_id = 01);
+            
+/* SUBQUERY EXAMPLE 2 */
+
+SELECT 
+    first_name,
+    last_name,
+    salary,
+    (SELECT 
+            AVG(salary) AS average_salary
+        FROM
+            employee_work_details)
+FROM
+    employee_work_details;
+
+/* SUBQUERY EXAMPLE 2 */
