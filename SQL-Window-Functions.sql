ac09ec4bb68119289d50d3d0ebf5ad59f69ca6fc
Johan Swan <jswannieman@gmail.com>
2025-02-01 13:48:11 +1300

    Update SQL-Window-Functions SQL Component

--- SQL-Window-Functions.sql
+++ SQL-Window-Functions.sql
@@ -1,6 +1,23 @@
 USE companyA;
 
+/* WINDOWS FUNCTIONS EXAMPLE 1 */
+
 SELECT 
 pers.first_name, pers.last_name, work.salary, AVG(salary) OVER(PARTITION BY pers.gender) as avg_salary 
+FROM employee_personal_details pers
+JOIN employee_work_details work ON personal_id = work_id;
+
+/* WINDOWS FUNCTIONS EXAMPLE 2 */
+
+SELECT 
+pers.first_name, pers.last_name, work.salary, SUM(salary) OVER(PARTITION BY pers.gender ORDER BY personal_id) as rolling_total 
+FROM employee_personal_details pers
+JOIN employee_work_details work ON personal_id = work_id;
+
+/* WINDOWS FUNCTIONS EXAMPLE 3 */
+
+SELECT 
+pers.first_name, pers.last_name, work.salary, 
+ROW_NUMBER() OVER() 
 FROM employee_personal_details pers
 JOIN employee_work_details work ON personal_id = work_id;
