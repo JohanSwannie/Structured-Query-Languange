7bc6027310fba1fbe7ff57a1bd2ae11c8ddf104f
Johan Swan <jswannieman@gmail.com>
2025-02-01 14:02:38 +1300

    Update SQL-Window-Functions SQL Component

--- SQL-Window-Functions.sql
+++ SQL-Window-Functions.sql
@@ -21,3 +21,11 @@ pers.first_name, pers.last_name, work.salary,
 ROW_NUMBER() OVER() 
 FROM employee_personal_details pers
 JOIN employee_work_details work ON personal_id = work_id;
+
+/* WINDOWS FUNCTIONS EXAMPLE 4 */
+
+SELECT 
+pers.first_name, pers.last_name, work.salary, 
+ROW_NUMBER() OVER(PARTITION BY gender) 
+FROM employee_personal_details pers
+JOIN employee_work_details work ON personal_id = work_id;
