663480531702530010c77fdeec86e5f262a05410
Johan Swan <jswannieman@gmail.com>
2025-02-08 09:55:07 +1300

    Update SQL-Stored-Procedures SQL Component

--- SQL-Stored-Procedures.sql
+++ SQL-Stored-Procedures.sql
@@ -1,8 +1,30 @@
 USE companyA;
 
 CREATE PROCEDURE older_employees()
-SELECT *
-FROM employee_personal_details
-WHERE age >= 30;
+SELECT 
+	*
+FROM 
+	employee_personal_details
+WHERE 
+	age >= 30;
 
 CALL older_employees();
+
+DELIMITER $$
+CREATE PROCEDURE high_low_salaries()
+BEGIN
+SELECT
+	* 
+FROM 
+	employee_work_details
+WHERE 
+	salary >= 105000;
+SELECT 
+    *
+FROM
+    employee_work_details
+WHERE
+    salary < 105000;
+END $$
+
+CALL high_low_salaries();
