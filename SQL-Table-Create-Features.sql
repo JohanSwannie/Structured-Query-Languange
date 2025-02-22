eeaff8d0547065113d721869f84461456e8cfe35
Johan Swan <jswannieman@gmail.com>
2025-02-23 10:40:20 +1300

    Update SQL-Table-Create-Features SQL Component

--- SQL-Table-Create-Features.sql
+++ SQL-Table-Create-Features.sql
@@ -7,9 +7,19 @@ USE companyb;
 DROP TABLE IF EXISTS employee_details1;
 
 CREATE TABLE IF NOT EXISTS employee_details1 (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     emp_name VARCHAR(40),
     emp_surname VARCHAR(40),
     emp_position VARCHAR(50),
     emp_salary INT NOT NULL
 );
+
+INSERT INTO employee_details1
+(emp_name, emp_surname, emp_position, emp_salary)
+VALUES ('James', 'Mower', 'Senior Project Manager', 185000),
+('Mary', 'Onsworth', 'Project Manager', 160000),
+('Koos', 'Roos', 'Senior Software Developer', 155000),
+('Dorothy', 'Blake', 'Software Developer', 125000),
+('Trudy', 'Brown', 'Data Analyst', 95000);
+
+DROP TABLE IF EXISTS employee_details2;
