276b566872ba47c1ce549155cd3802d699092010
Johan Swan <jswannieman@gmail.com>
2025-02-23 09:49:53 +1300

    Update SQL-Table-Create-Features SQL Component

--- SQL-Table-Create-Features.sql
+++ SQL-Table-Create-Features.sql
@@ -4,5 +4,12 @@ CREATE DATABASE IF NOT EXISTS companyb;
 
 USE companyb;
 
-DROP TABLE IF EXISTS employee_details;
+DROP TABLE IF EXISTS employee_details1;
 
+CREATE TABLE IF NOT EXISTS employee_details1 (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    emp_name VARCHAR(40),
+    emp_surname VARCHAR(40),
+    emp_position VARCHAR(50),
+    emp_salary INT NOT NULL
+);
