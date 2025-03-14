71ef70a6b7fe9ce780e4042b42d7d2d364d1b1ab
Johan Swan <jswannieman@gmail.com>
2025-03-15 11:02:58 +1300

    Update SQL-Stored-Procedures SQL Component

--- SQL-Stored-Procedures.sql
+++ SQL-Stored-Procedures.sql
@@ -1,5 +1,7 @@
 USE companyA;
 
+DROP PROCEDURE IF EXISTS older_employees;
+
 CREATE PROCEDURE older_employees()
 SELECT 
 	*
@@ -9,6 +11,8 @@ WHERE
 	age >= 30;
 
 CALL older_employees();
+
+DROP PROCEDURE IF EXISTS high_low_salaries;
 
 DELIMITER $$
 CREATE PROCEDURE high_low_salaries()
@@ -26,5 +30,25 @@ FROM
 WHERE
     salary < 105000;
 END $$
+DELIMITER ;
 
 CALL high_low_salaries();
+
+DROP PROCEDURE IF EXISTS specific_employee_salary;
+
+DELIMITER $$
+CREATE PROCEDURE specific_employee_salary(wrk_id INT)
+BEGIN
+SELECT
+	first_name,
+    last_name,
+	salary as specific_salary
+FROM 
+	employee_work_details
+WHERE 
+	work_id = wrk_id;
+END $$
+DELIMITER ;
+
+CALL specific_employee_salary(02);
+
